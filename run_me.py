4dbe73a9dd51b708a14a6c0ebd6ebb62bd3698fb
dgokduman <dogukangokdumann@gmail.com>
2024-04-08 18:58:21 +0300

    initial commit

---
+++ run_me.py
@@ -0,0 +1,17 @@
+from Detector import *
+
+modelURL ="http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
+#modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d4_coco17_tpu-32.tar.gz"
+#modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet101_v1_640x640_coco17_tpu-8.tar.gz"
+
+classFile = "coco.names"
+imagePath = "test/2.jpg"
+videoPath = "test/01.mp4"
+threshold = 0.5
+
+detector = Detector()
+detector.readClassses(classFile)
+detector.downloadModel(modelURL)
+detector.loadModel()
+#detector.predictImage(imagePath, threshold)
+detector.predictVideo(videoPath, threshold)
